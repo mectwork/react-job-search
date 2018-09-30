f1c2eb754646fd7f4292accbf33faa33aa65a958
Manuel E Carpio <mectwork@gmail.com>
2018-09-30 00:56:23 -0400

    Added axios and called job API to show search results

--- app/src/App.js
+++ app/src/App.js
@@ -1,16 +1,50 @@
 import React, { Component } from "react";
-import './App.css';
+import "./App.css";
 import Navbar from "./components/layout/Navbar";
 import SearchForm from "./components/search/SearchForm";
+import axios from "axios";
 
 class App extends Component {
+  state = {
+    jobs: []
+  };
+
+  jobsList = search => {
+    const { what, where } = search;
+    const url = `https://cors-anywhere.herokuapp.com/https://authenticjobs.com/api/?api_key=${
+      process.env.REACT_APP_API_KEY
+    }&format=json&method=aj.jobs.search&keywords=${what}&location=${where}&perpage=10`;
+    console.log(url);
+    axios
+      .post(url)
+      .then(res => {
+        if (res.data.stat === "fail") {
+          console.log(res.data.desc);
+          return;
+        }
+        console.log(res.data);
+        const jobs = res.data.listings.listing;
+        this.setState({ jobs });
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  };
+
   render() {
+    console.log(this.state);
     return (
       <React.Fragment>
         <Navbar />
         <div className="container">
           <h1>Job Search</h1>
-          <SearchForm />
+          <SearchForm jobSearch={this.jobsList} />
+          <hr />
+          <ul>
+            {this.state.jobs.map(job => (
+              <li key={job.id}>{job.title}</li>
+            ))}
+          </ul>
         </div>
       </React.Fragment>
     );
--- app/src/components/search/SearchForm.js
+++ app/src/components/search/SearchForm.js
@@ -1,36 +1,48 @@
 import React, { Component } from "react";
 
 export default class SearchForm extends Component {
+  state = {
+    what: "",
+    where: ""
+  };
+  handleOnSubmit = e => {
+    e.preventDefault();
+    const { what, where } = this.state;
+    this.props.jobSearch({ what, where });
+  };
+
+  handleOnChange = e => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
+
   render() {
     return (
-      <form className="row mt-4" id="search-job">
-        <div class="input-group">
+      <form onSubmit={this.handleOnSubmit} className="row mt-4" id="search-job">
+        <div className="input-group">
           <div className="col-md-5 mb-3">
             <label htmlFor="what">What:</label>
             <input
               className="form-control"
-              type="search"
               id="what"
               name="what"
               placeholder="job title, keywords, or company"
-              aria-label="What"
+              value={this.state.what}
+              onChange={this.handleOnChange}
             />
           </div>
           <div className="col-md-5 mb-3">
             <label htmlFor="where">Where:</label>
             <input
               className="form-control"
-              type="search"
               placeholder="city, state, or zip code"
-              aria-label="Search"
               id="where"
+              name="where"
+              value={this.state.where}
+              onChange={this.handleOnChange}
             />
           </div>
           <div className="col-md-2 mb-3 mt-2">
-            <button
-              className="btn btn-block btn-outline-success"
-              type="submit"
-            >
+            <button className="btn btn-block btn-outline-success" type="submit">
               Search
             </button>
           </div>
