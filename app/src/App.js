0122f0cdd4285d072d76c315267d4ac294e32aaa
Manuel E Carpio <mectwork@gmail.com>
2018-09-30 11:03:10 -0400

    Added spinner when loading results.

--- app/src/App.js
+++ app/src/App.js
@@ -2,29 +2,36 @@ import React, { Component } from "react";
 import "./App.css";
 import Navbar from "./components/layout/Navbar";
 import SearchForm from "./components/search/SearchForm";
+import { BarLoader } from "react-spinners";
 import axios from "axios";
 
 class App extends Component {
   state = {
-    jobs: []
+    jobs: [],
+    loading: false
   };
 
   jobsList = search => {
+    this.setState({ ...this.state, loading: true });
     const { what, where } = search;
     const url = `https://cors-anywhere.herokuapp.com/https://authenticjobs.com/api/?api_key=${
       process.env.REACT_APP_API_KEY
-    }&format=json&method=aj.jobs.search&keywords=${what}&location=${where}&perpage=10`;
-    console.log(url);
+    }&format=json&method=aj.jobs.search&keywords=${what
+      .split(" ")
+      .join("+")}&location=${where.split(" ").join("+")}&perpage=15`;
+
     axios
       .post(url)
       .then(res => {
+        this.setState({ ...this.state, loading: true });
+
         if (res.data.stat === "fail") {
           console.log(res.data.desc);
           return;
         }
         console.log(res.data);
         const jobs = res.data.listings.listing;
-        this.setState({ jobs });
+        this.setState({ ...this.state, jobs: jobs, loading: false });
       })
       .catch(err => {
         console.log(err);
@@ -39,6 +46,14 @@ class App extends Component {
         <div className="container">
           <h1>Job Search</h1>
           <SearchForm jobSearch={this.jobsList} />
+          <div className="text-center">
+            <BarLoader
+              widthUnit={"%"}
+              width={100}
+              color={"#28a745"}
+              loading={this.state.loading}
+            />
+          </div>
           <hr />
           <ul>
             {this.state.jobs.map(job => (
